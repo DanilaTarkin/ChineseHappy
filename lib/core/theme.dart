83f591a15326ba8f9dbbc1f08dcc4eddf809a950
Tarkin <danil200901@mail.ru>
2022-04-20 20:38:44 +0500

    add constants and theme

---
+++ lib/core/theme.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+ThemeData basicTheme = ThemeData(
+  fontFamily: 'Segoe-UI');
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-
+import 'package:happychinese/core/theme.dart';
 import 'core/navigation.dart';
 
 void main() {
   runApp(
-      const MaterialApp(debugShowCheckedModeBanner: false, home: Navigation()));
+      MaterialApp(
+        theme: basicTheme,
+        debugShowCheckedModeBanner: false, 
+        home: Navigation()));
 }
